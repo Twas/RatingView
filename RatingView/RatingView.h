ba70e0d7cec7bb46b51114f36bae49cbc5128431
Evgeniy Leychenko <e.leychenko@theappsolutions.com>
2018-11-16 13:35:11 +0200

    initial commit

---
+++ RatingView/RatingView.h
@@ -0,0 +1,19 @@
+//
+//  RatingView.h
+//  RatingView
+//
+//  Created by Evgeniy Leychenko on 11/16/18.
+//  Copyright © 2018 TAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RatingView.
+FOUNDATION_EXPORT double RatingViewVersionNumber;
+
+//! Project version string for RatingView.
+FOUNDATION_EXPORT const unsigned char RatingViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RatingView/PublicHeader.h>
+
+
---
+++ RatingViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RatingViewDemo
+//
+//  Created by Evgeniy Leychenko on 11/16/18.
+//  Copyright © 2018 TAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
